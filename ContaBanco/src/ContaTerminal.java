343dd39a1f7afa1b82ceaeb921c2a97af927e60a
pExola <kauan126paixao@gmail.com>
2024-05-05 12:51:17 -0300

    Conta Terminal v1

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       Scanner leia = new Scanner(System.in);
+       System.out.println("Insira o número da agência: ");
+       String agencia = leia.nextLine();
+
+       System.out.println("Insira o número da Conta: ");
+       int conta = leia.nextInt();
+
+       System.out.println("Insira seu saldo: ");
+       Double saldo = leia.nextDouble();
+
+       System.out.println("Insira seu nome: ");
+       String nome = leia.next();
+       
+       System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " , conta " + conta + " e seu saldo " + saldo + " já está disponível para saque");
     }
 }
